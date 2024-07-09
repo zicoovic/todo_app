b0d870f91b846b2f5400c0b0033b432ed810449d
zicoovic <abdelrhmanzakariaqa@gmail.com>
2024-07-09 08:54:01 +0300

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+import 'models/todo.dart';
+import 'todo_list.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String _email = '';
+
+  final List<Todo> todos = [
+    const Todo(
+        title: 'Buy milk',
+        description: 'There is no milk left in the fridge!',
+        priority: Priority.high),
+    const Todo(
+        title: 'Make the bed',
+        description: 'Keep things tidy please..',
+        priority: Priority.low),
+    const Todo(
+        title: 'Pay bills',
+        description: 'The gas bill needs paying ASAP.',
+        priority: Priority.urgent),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo List'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[200],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Expanded(child: TodoList(todos: todos)),
+            // form stuff below here
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              // obscureText: true,
+              decoration: const InputDecoration(
+                label: Text('Email address'),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _email = value.trim();
+                });
+              },
+            ),
+            const SizedBox(height: 20),
+            Text('Your email: $_email'),
+          ],
+        ),
+      ),
+    );
+  }
+}
